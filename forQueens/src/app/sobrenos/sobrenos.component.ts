ccbe955d42f82c30c5d5ec493f8eee668737bf77
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-06-07 20:35:26 -0300

    Animações na página home e sobre-nós

--- forQueens/src/app/sobrenos/sobrenos.component.ts
+++ forQueens/src/app/sobrenos/sobrenos.component.ts
@@ -11,6 +11,25 @@ export class SobrenosComponent implements OnInit {
 
   ngOnInit(): void {
     window.scroll(0, 0)
+
+    const target = document.querySelectorAll('[data-anime]');
+    const animationClass = 'animate';
+
+    function animeScroll() {
+      const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+      target.forEach((element) => {
+        if ((windowTop) > (element as HTMLElement).offsetTop) {
+          element.classList.add(animationClass);
+        } else {
+          element.classList.remove(animationClass);
+        }
+
+      })
+    }
+
+    window.addEventListener('scroll', () => {
+      animeScroll()
+    })
   }
 
 }
