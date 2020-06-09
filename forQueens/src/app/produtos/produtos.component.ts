536a106b94acd3665aa4fdee8fdbce76e09032ae
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-06-09 02:47:59 -0300

    Grandes melhorias na página produtos

--- forQueens/src/app/produtos/produtos.component.ts
+++ forQueens/src/app/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faShoppingBag, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-produtos',
@@ -7,10 +8,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosComponent implements OnInit {
 
+  faShoppingBag = faShoppingBag
+  faShoppingCart = faShoppingCart
+
   constructor() { }
 
   ngOnInit(): void {
     window.scroll(0, 0)
+
+  
+  }
+
+  escolhaTodos() {
+    const todos = document.querySelector("#todos")
+    const botox = document.querySelector("#botox")
+    const hidratante = document.querySelector("#hidratante")
+    const mascara = document.querySelector("#mascara")
+
+    const produtoTodos = document.getElementById("produtoTodos")
+
+    const catEscolhida = 'bg-active';
+
+    if (todos.classList.contains(catEscolhida)) {
+      todos.classList.remove(catEscolhida)
+    } else {
+      todos.classList.add(catEscolhida)
+      botox.classList.remove(catEscolhida)
+      hidratante.classList.remove(catEscolhida)
+      mascara.classList.remove(catEscolhida)
+
+      produtoTodos.style.display = "block"
+    }
+  }
+
+  escolhaBotox() {
+    const todos = document.querySelector("#todos")
+    const botox = document.querySelector("#botox")
+    const hidratante = document.querySelector("#hidratante")
+    const mascara = document.querySelector("#mascara")
+
+    const produtoTodos = document.getElementById("produtoTodos")
+
+    const catEscolhida = 'bg-active';
+
+    if (botox.classList.contains(catEscolhida)) {
+      botox.classList.remove(catEscolhida)
+    } else {
+      todos.classList.remove(catEscolhida)
+      botox.classList.add(catEscolhida)
+      hidratante.classList.remove(catEscolhida)
+      mascara.classList.remove(catEscolhida)
+
+      produtoTodos.style.display = "none"
+    }
+      
+
+  }
+
+  escolhaHidratante() {
+    const todos = document.querySelector("#todos")
+    const botox = document.querySelector("#botox")
+    const hidratante = document.querySelector("#hidratante")
+    const mascara = document.querySelector("#mascara")
+
+    const produtoTodos = document.getElementById("produtoTodos")
+
+    const catEscolhida = 'bg-active';
+
+    if (hidratante.classList.contains(catEscolhida)) {
+      hidratante.classList.remove(catEscolhida)
+    } else {
+      todos.classList.remove(catEscolhida)
+      botox.classList.remove(catEscolhida)
+      hidratante.classList.add(catEscolhida)
+      mascara.classList.remove(catEscolhida)
+
+      produtoTodos.style.display = "none"
+    }
+  }
+
+  escolhaMascara() {
+    const todos = document.querySelector("#todos")
+    const botox = document.querySelector("#botox")
+    const hidratante = document.querySelector("#hidratante")
+    const mascara = document.querySelector("#mascara")
+
+    const produtoTodos = document.getElementById("produtoTodos")
+
+    const catEscolhida = 'bg-active';
+
+    if (mascara.classList.contains(catEscolhida)) {
+      mascara.classList.remove(catEscolhida)
+    } else {
+      todos.classList.remove(catEscolhida)
+      botox.classList.remove(catEscolhida)
+      hidratante.classList.remove(catEscolhida)
+      mascara.classList.add(catEscolhida)
+
+      produtoTodos.style.display = "none"
+    }
   }
 
 }
