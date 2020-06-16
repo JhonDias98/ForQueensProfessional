51293de6f903d9f1de055293109ee810c2f8a54b
Willian Jhonatan <61021544+willian1995jhonatan@users.noreply.github.com>
2020-06-16 15:08:54 -0300

    Informações adicionais

--- forQueens/src/app/mapa-site/mapa-site.component.ts
+++ forQueens/src/app/mapa-site/mapa-site.component.ts
@@ -10,6 +10,26 @@ export class MapaSiteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.accordion()
+    window.scroll(0, 0)
+  }
+
+  accordion() {
+    let acc = document.getElementsByClassName("accordion_mapasite");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
   }
 
 }
+
