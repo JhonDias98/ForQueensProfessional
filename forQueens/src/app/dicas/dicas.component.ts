eeef2cc4eee3c2691042964fe621b7b884c4d5ea
RenanZL <renanzago@gmail.com>
2020-06-29 03:42:43 -0300

    video tentativa

--- forQueens/src/app/dicas/dicas.component.ts
+++ forQueens/src/app/dicas/dicas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-dicas',
@@ -6,11 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dicas.component.css']
 })
 export class DicasComponent implements OnInit {
-
+  modal = document.querySelector("modal");
   constructor() { }
-
+  @ViewChild("essenceLiss", { static: false }) essenceLiss: ElementRef;
+  @ViewChild("qNano", { static: false }) qNano: ElementRef;
+  @ViewChild("nutritionRose", { static: false }) nutritionRose: ElementRef;
+  @ViewChild("qqCream", { static: false }) qqCream: ElementRef;
   ngOnInit(): void {
     window.scroll(0, 0)
   }
-
+  essence_liss(){
+    const backdrop2 = document.getElementById("backdrop2");
+    backdrop2.style.display="block"
+    this.essenceLiss.nativeElement.play();
+  }
+  q_nano(){
+    const backdrop2 = document.getElementById("backdrop2");
+    backdrop2.style.display="block"
+    this.qNano.nativeElement.play();
+  }
+  nutrition_rose(){
+    const backdrop2 = document.getElementById("backdrop2");
+    backdrop2.style.display="block"
+    this.nutritionRose.nativeElement.play();
+  }
+  qq_cream(){
+    const backdrop2 = document.getElementById("backdrop2");
+    backdrop2.style.display="block"
+    this.qqCream.nativeElement.play();
+  }
+  pausa_video(){
+    this.qqCream.nativeElement.pause();
+    this.nutritionRose.nativeElement.pause();
+    this.qNano.nativeElement.pause();
+    this.essenceLiss.nativeElement.pause();
+    const backdrop2 = document.getElementById("backdrop2");
+    backdrop2.style.display="none"
+  }
 }
